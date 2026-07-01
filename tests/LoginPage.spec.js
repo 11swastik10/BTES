5676df317a615504cf823881152422247cf4d700
Swastik Chauhan <swastikrana11102003@gmail.com>
2026-07-01 09:41:00 +0530

    This is my first commit

---
+++ tests/LoginPage.spec.js
@@ -0,0 +1,59 @@
+const { test } = require('@playwright/test');
+
+const Login = require('../pages/login');
+const AddToCart = require('../pages/addtocart');
+
+test('Login functionality', async ({ page }) => {
+
+    const login = new Login(page);
+
+    await login.goto('');
+
+    await login.loginApplication(
+        'standard_user',
+        'secret_sauce'
+    );
+
+});
+
+
+test('Add product to cart', async ({ page }) => {
+
+    const login = new Login(page);
+    const cart = new AddToCart(page);
+
+    await login.goto('');
+
+    await login.loginApplication(
+        'standard_user',
+        'secret_sauce'
+    );
+
+    await cart.addProduct();
+
+});
+
+
+test('Checkout functionality', async ({ page }) => {
+
+    const login = new Login(page);
+    const cart = new AddToCart(page);
+
+    await login.goto('');
+
+    await login.loginApplication(
+        'standard_user',
+        'secret_sauce'
+    );
+
+    await cart.addProduct();
+
+    await cart.checkoutDetails(
+        'John',
+        'Doe',
+        '12345'
+    );
+
+    await cart.verifyOrderPlaced();
+
+});
---
+++ tests/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+  // await login.goto('');
